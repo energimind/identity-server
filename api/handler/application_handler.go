240ddc4b55473ace997407a4716b7c81e42dc7c3
Alan Bunjevac <alan.bunjevac@gmail.com>
2024-01-23 19:00:05 +0100

    Application handler skeleton

---
+++ api/handler/application_handler.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/energimind/identity-service/appl/service"
+	"github.com/gin-gonic/gin"
+)
+
+// ApplicationHandler is a REST API handler for managing applications.
+type ApplicationHandler struct {
+	service *service.ApplicationService
+}
+
+// NewApplicationHandler creates a new ApplicationHandler.
+func NewApplicationHandler(service *service.ApplicationService) *ApplicationHandler {
+	return &ApplicationHandler{service: service}
+}
+
+// Bind binds the ApplicationHandler to a root provided by a router.
+func (h *ApplicationHandler) Bind(root gin.IRoutes) {
+	root.GET("", h.findAll)
+	root.GET("/:id", h.findByID)
+	root.POST("", h.create)
+	root.PUT("/:id", h.update)
+	root.DELETE("/:id", h.delete)
+}
+
+func (h *ApplicationHandler) findAll(c *gin.Context) {
+	c.Status(http.StatusNotImplemented)
+}
+
+func (h *ApplicationHandler) findByID(c *gin.Context) {
+	c.Status(http.StatusNotImplemented)
+}
+
+func (h *ApplicationHandler) create(c *gin.Context) {
+	c.Status(http.StatusNotImplemented)
+}
+
+func (h *ApplicationHandler) update(c *gin.Context) {
+	c.Status(http.StatusNotImplemented)
+}
+
+func (h *ApplicationHandler) delete(c *gin.Context) {
+	c.Status(http.StatusNotImplemented)
+}
