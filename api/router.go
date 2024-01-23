359abadd04c8bd17478b47ea1f9f3041df044bf9
Alan Bunjevac <alan.bunjevac@gmail.com>
2024-01-23 19:00:25 +0100

    API router

---
+++ api/router.go
@@ -0,0 +1,65 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type handler interface {
+	Bind(root gin.IRoutes)
+}
+
+// Config is the configuration for the router.
+type Config struct {
+	AllowOrigin string
+}
+
+// Handlers is a collection of handler that will be bound to the router.
+type Handlers struct {
+	ApplicationHandler handler
+	HealthHandler      handler
+}
+
+// Router is the router for the REST API.
+type Router struct {
+	rtr *gin.Engine
+}
+
+// NewRouter creates a new Router.
+func NewRouter(config Config, handlers Handlers) *Router {
+	return &Router{
+		rtr: configure(config, handlers),
+	}
+}
+
+// ServeHTTP implements the http.Handler interface.
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	r.rtr.ServeHTTP(w, req)
+}
+
+// GetRoutes returns the routes of the router.
+func (r *Router) GetRoutes() []gin.RouteInfo {
+	return r.rtr.Routes()
+}
+
+func configure(config Config, handlers Handlers) *gin.Engine {
+	rtr := gin.New()
+
+	rtr.Use(gin.Logger())
+	rtr.Use(cors(config.AllowOrigin))
+
+	admin := rtr.Group("/api/v1/admin")
+	{
+		admin.Use(requireActor())
+
+		handlers.ApplicationHandler.Bind(admin.Group("/applications"))
+	}
+
+	health := rtr.Group("/health")
+	{
+		handlers.HealthHandler.Bind(health)
+	}
+
+	return rtr
+}
